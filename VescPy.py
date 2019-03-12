a54ab283a5d9213781a774399f76f8382864cb8a
Benjamin Bussell <BSBussell@users.noreply.github.com>
2019-03-12 13:03:51 -0500

    Warning will not work with any older versions

--- VescPy.py
+++ VescPy.py
@@ -49,8 +49,8 @@ class VESC:
 
         '''
 
-
-
+        bytes([167, 105])
+        
         self.ardu.write(("T"+str(chr(self.throttleValue))).encode())
         self.ardu.write(("S"+str(chr(self.angleValue))).encode())
 
